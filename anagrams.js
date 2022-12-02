6cf613e0b720cb7183a0d1d337e5d819ee7773c4
Seedname <45999377+Seedname@users.noreply.github.com>
2022-12-01 23:43:18 -0500

    Update anagrams.js

--- anagrams.js
+++ anagrams.js
@@ -202,7 +202,7 @@ var time3;
 var notifText;
 var isError;
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight); 
+    createCanvas(document.body.clientWidth, document.body.clientHeight); 
 
     lists = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
     lists2 = [a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1, m1, n1, o1, p1, q1, r1, s1, t1, u1, v1, w1, x1, y1, z1];
